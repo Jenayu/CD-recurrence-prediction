70a8e0a60166a4b00ebaa4db5234d0bf20f92957
Jennifer Chen <32661544+Jenayu@users.noreply.github.com>
2019-06-10 12:35:24 -0400

    Update featureSelection.py

--- featureSelection.py
+++ featureSelection.py
@@ -26,14 +26,14 @@ def featureSel_kbest(train_data, num_features):
 
 # feature selection (rfe)
 
-def featureSel_rfe(train_data, num_features):
+
+def featureSel_rfe(train_data, estimator, num_features):
     
     train_data_nonconst = train_data.loc[:, (train_data!= train_data.iloc[0]).any()] 
 
-    estimator = SVR(kernel="linear")
     selector = RFE(estimator, num_features, step=1)
 
-    train_features = train_data_nonconst.drop(columns=['Patient','Recurrence'],axis=1)
+    train_features = train_data_nonconst.drop(columns=['Recurrence'],axis=1)
     train_feature_names = train_features.columns
     train_labels = [1 if x=="recurrence" else 0 for x in train_data['Recurrence']] 
     
@@ -46,14 +46,13 @@ def featureSel_rfe(train_data, num_features):
 
 # feature selection (rfecv)
 
-def featureSel_rfecv(train_data, num_folds):
+def featureSel_rfecv(train_data, estimator, num_folds):
     
     train_data_nonconst = train_data.loc[:, (train_data!= train_data.iloc[0]).any()] 
 
-    estimator = SVR(kernel="linear")
     selector = RFECV(estimator, step=1, cv=num_folds)
 
-    train_features = train_data_nonconst.drop(columns=['Patient','Recurrence'],axis=1)
+    train_features = train_data_nonconst.drop(columns=['Recurrence'],axis=1)
     train_feature_names = train_features.columns
     train_labels = [1 if x=="recurrence" else 0 for x in train_data['Recurrence']] 
     
@@ -62,7 +61,6 @@ def featureSel_rfecv(train_data, num_folds):
     selected_feature_names_rfecv = train_feature_names[selector.support_]
     
     return list(selected_feature_names_rfecv)
-
 
 
 
